cf4be6b216c4190d576bb28e1676d53cf3f25c96
Adam Shelley <adam.shelley18@gmail.com>
2020-01-26 17:22:56 +0000

    fixed login error2

--- controllers/usertodos-controllers.js
+++ controllers/usertodos-controllers.js
@@ -159,7 +159,7 @@ const login = async (req, res, next) => {
   try {
     token = jwt.sign(
       { userId: existingUser.id, email: existingUser.email },
-      proces.env.KEY,
+      process.env.JWT_KEY,
       { expiresIn: "1h" }
     );
   } catch (err) {
